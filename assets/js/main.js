dd28250edcc70a935be60d85af8b03ae38c39461
CipiVlad <supercoder.cipi@gmail.com>
2022-05-11 12:41:04 +0200

    lvls

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+// console.log("text");
+
+
+// length
+
+// lev1_1
+
+let nachName = "Ciprian";
+let vorName = "Vlad";
+let name = nachName + vorName;
+console.log(nachName.length);
+console.log(vorName.length);
+console.log(name.length);
+
+// indexOf
+// lev1_2
+
+let txt = "How inappropriate to call this planet Earth, when clearly it is Ocean.";
+console.log(txt.indexOf("h"));
+console.log(txt.indexOf("Earth"));
+console.log(txt.indexOf("Moon"));
